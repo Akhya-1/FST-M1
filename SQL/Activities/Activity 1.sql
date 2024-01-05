127f9fd28437c553700e7fe0859ccbec5468539d
Akhya-1 <155718911+Akhya-1@users.noreply.github.com>
2024-01-05 18:56:34 +0530

    Add files via upload

---
+++ SQL/Activities/Activity 1.sql
@@ -0,0 +1,9 @@
+REM   Script: Activity 1
+REM   activity 1
+
+CREATE TABLE tableName;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman (salesman_id int,salesman_name varchar(20),salesman_city varchar(20),commission int);
+
---
+++ SQL/Activities/Activity 4.sql
@@ -0,0 +1,22 @@
+REM   Script: Activity 4
+REM   activity 4
+
+CREATE TABLE tableName;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman (salesman_id int,salesman_name varchar(20),salesman_city varchar(20),commission int);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+SELECT * FROM salesman;
+
